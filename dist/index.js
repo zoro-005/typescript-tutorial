977beb43bf31b8efb3815aa98442bb44b0580ccd
zoro-005 <tdevarya@gmail.com>
2025-08-31 16:59:23 +0530

    enums, oop start

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+"use strict";
+// let sales = 123_456_789;
+// let course = 'TypeScript'
+// let is_published = true
+// let level;
+Object.defineProperty(exports, "__esModule", { value: true });
+// function render(document: any){
+//     console.log(document);
+// }
+// let numbers: number[] = [1, 2, 3];
+// let user: [number, string] = [1, 'User']
+// console.log(user[0]);
+// console.log(user[1]);
+// enum Size  {Small = 99 , Medium, Large}
+// let mySize: Size = Size.Large;
+// console.log(mySize);
+// function calculateTax (income: number, tax_year = 2022): number{
+//     if (tax_year < 2022){
+//         return income * 1.2
+//     }
+//     return income * 1.3
+// }
+// calculateTax(50_000, 2025);
+let employee = { id: 1, emp_name: "noob", retire: (date) => { console.log(date); } };
+employee.emp_name = "pro";
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+// let sales = 123_456_789;
+// let course = 'TypeScript'
+// let is_published = true
+// let level;
+
+// function render(document: any){
+//     console.log(document);
+// }
+
+// let numbers: number[] = [1, 2, 3];
+
+// let user: [number, string] = [1, 'User']
+// console.log(user[0]);
+// console.log(user[1]);
+// enum Size  {Small = 99 , Medium, Large}
+// let mySize: Size = Size.Large;
+// console.log(mySize);
+// function calculateTax (income: number, tax_year = 2022): number{
+//     if (tax_year < 2022){
+//         return income * 1.2
+//     }
+//     return income * 1.3
+// }
+// calculateTax(50_000, 2025);
+
+let employee: {
+    readonly id: number,
+    emp_name: string,
+    retire: (date: Date) => void
+} = {id: 1, emp_name : "noob", retire: (date: Date) => {console.log(date)}};
+employee.emp_name = "pro"
